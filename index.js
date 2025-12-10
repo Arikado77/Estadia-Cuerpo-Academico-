591c9068fcabaef32b528aadfe89da3ca2c72777
Arikado77 <carlosarikado19@gmail.com>
2025-12-09 23:32:12 -0600

    act16

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // para cualquier ruta devolver index.html (single page apps)
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
