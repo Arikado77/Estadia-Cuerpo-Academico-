be143f2c8754714892a5c192c2e694c828b9b8fa
Arikado77 <carlosarikado19@gmail.com>
2025-12-10 01:01:21 -0600

    act19

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const PORT = process.env.PORT || 3000;
 // servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Opción 2: Usar app.use() para capturar todas las rutas (el fallback)
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => console.log('Server listening on port', PORT));
